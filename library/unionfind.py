8d0d2f626536f7d3fb98afa7cfb8ec7f4294d2d8
Morisaki Mikiya <git@morisakimikiya.com>
2021-07-12 12:19:35 +0000

    Update unionfind

--- library/unionfind.py
+++ library/unionfind.py
@@ -1,10 +1,13 @@
-class UnionFind():
+from collections import defaultdict
+
+
+class UnionFind:
     def __init__(self, n):
-        self.parents = list(range(n))
-        self.rank = [0] * n
+        self.n = n
+        self.parents = [-1] * n
 
     def find(self, x):
-        if self.parents[x] == x:
+        if self.parents[x] < 0:
             return x
         else:
             self.parents[x] = self.find(self.parents[x])
@@ -17,9 +20,33 @@ class UnionFind():
         if x == y:
             return
 
-        if self.rank[x] < self.rank[y]:
-            self.parents[x] = y
-        else:
-            self.parents[y] = x
-            if self.rank[x] == self.rank[y]:
-                self.rank[x] += 1
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    def size(self, x):
+        return -self.parents[self.find(x)]
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    def group_count(self):
+        return len(self.roots())
+
+    def all_group_members(self):
+        group_members = defaultdict(list)
+        for member in range(self.n):
+            group_members[self.find(member)].append(member)
+        return group_members
+
+    def __str__(self):
+        return "\n".join(f"{r}: {m}" for r, m in self.all_group_members().items())
