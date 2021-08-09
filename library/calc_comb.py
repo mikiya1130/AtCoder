2f319da3ea38ee867de1971d25de39dc37314329
Morisaki Mikiya <git@morisakimikiya.com>
2021-08-09 07:24:45 +0000

    Update library
    Add homogeneous polynomial
    Add assert

--- library/calc_comb.py
+++ library/calc_comb.py
@@ -4,6 +4,8 @@ from functools import reduce
 
 
 def cmb(n, r):
+    assert n >= r >= 0
+
     r = min(n - r, r)
     if r == 0:
         return 1
@@ -11,11 +13,13 @@ def cmb(n, r):
     under = reduce(mul, range(1, r + 1))
     return over // under
 
+
 # ------------------------------
 
+
 def cmb(n, r, mod):
-    if r < 0 or r > n:
-        return 0
+    assert n >= r >= 0
+
     r = min(r, n - r)
     return g1[n] * g2[r] * g2[n - r] % mod
 
@@ -35,9 +39,19 @@ cmb(n, r, mod)
 
 # ------------------------------
 
-def cmb(n, r, mod=10**9+7):
+
+def cmb(n, r, mod=10 ** 9 + 7):
+    assert n >= r >= 0
+
     p, q = 1, 1
     for i in range(r):
         p = p * (n - i) % mod
         q = q * (i + 1) % mod
     return p * pow(q, mod - 2, mod) % mod
+
+
+# ------------------------------
+
+# 重複組み合わせ
+def H(n, r, mod=10 ** 9 + 7):
+    return cmb(n + r - 1, r, mod)
