aaa60f7871e961275edbc8a76062728ef71198a2
Morisaki Mikiya <git@morisakimikiya.com>
2021-08-09 01:08:07 +0000

    Fix library
    make_divisors.py

--- library/make_divisors.py
+++ library/make_divisors.py
@@ -1,18 +1,11 @@
 # 約数
-def factorization(n):
-    arr = []
-    temp = n
-    for i in range(2, int(-(-(n ** 0.5) // 1)) + 1):
-        if temp % i == 0:
-            cnt = 0
-            while temp % i == 0:
-                cnt += 1
-                temp //= i
-            arr.append([i, cnt])
-    if temp != 1:
-        arr.append([temp, 1])
-
-    if arr == []:
-        arr.append([n, 1])
-
-    return arr
+def make_divisors(n):
+    lower_divisors, upper_divisors = [], []
+    i = 1
+    while i * i <= n:
+        if n % i == 0:
+            lower_divisors.append(i)
+            if i != n // i:
+                upper_divisors.append(n // i)
+        i += 1
+    return lower_divisors + upper_divisors[::-1]
